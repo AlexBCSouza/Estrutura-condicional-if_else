b9149f9e792f0b69608999a02f90d981036980c7
AlexBCSouza <alex.batista.91@gmail.com>
2022-06-20 00:49:58 -0300

    Exercício 2 if_else Fórmula de bhaskara

---
+++ src/Condição_if_else/Test_2.java
@@ -0,0 +1,29 @@
+package Condição_if_else;
+
+import java.sql.SQLOutput;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Test_2 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+
+        double delta = b * b - 4.0 * a * c;
+
+        if (a == 0 || delta < 0.0){
+            System.out.println("Impossível culcular.");
+        }
+        else {
+            double r1 = (-b + Math.sqrt(delta)) / (2 * a);
+            double r2 = (-b - Math.sqrt(delta)) / (2 * a);
+            System.out.printf("R1 = %.5f%n", r1);
+            System.out.printf("R2 = %.5f%n", r2);
+        }
+        sc.close();
+    }
+}
