fb4845d2dc5cf3cfd7847ddf1c978bae6dc2cf69
AlexBCSouza <alex.batista.91@gmail.com>
2022-06-19 23:28:10 -0300

    Exercício 1 if_else

---
+++ src/Condição_if_else/Test_1.java
@@ -0,0 +1,26 @@
+package Condição_if_else;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Test_1 {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double nota_1 = sc.nextDouble();
+        double nota_2 = sc.nextDouble();
+        double notaFinal = nota_1 + nota_2;
+
+        System.out.printf("Nota final = %.1f%n", notaFinal);
+
+        if (notaFinal < 60 ){
+            System.out.println(" Reprovado! ");
+        }
+        else {
+            System.out.println("Aprovado!");
+        }
+        sc.close();
+    }
+}
