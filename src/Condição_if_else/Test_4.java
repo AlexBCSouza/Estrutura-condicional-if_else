bcbedb6f576533d028e4667e4289cf25638fdfc8
AlexBCSouza <alex.batista.91@gmail.com>
2022-07-11 22:46:43 -0300

    Exercício 3 if_else

---
+++ src/Condição_if_else/Test_4.java
@@ -0,0 +1,16 @@
+package Condição_if_else;
+
+public class Test_4 {
+    public static void main(String[] args) {
+
+         double b, B, h, area;
+
+         b = 5.0;
+         B = 2.0;
+         h = 6.0;
+
+        area = (b + B)/2 * h;
+
+        System.out.println("Area do trapézio = " + area);
+    }
+}
